06753b1a0cf1b91ea8236629eef7070380631fdc
jasonpark <jasonpark0503@gmail.com>
2023-02-02 13:58:06 +0900

    Design: ThemeProvider 초기 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import theme from "./style/theme";
 import GlobalStyle from "./style/GlobalStyle";
+
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyle>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </GlobalStyle>
   </React.StrictMode>
 );
---
+++ src/style/theme.js
@@ -0,0 +1,13 @@
+const size = {
+  mobile: "767px",
+  desktop: "768px",
+};
+
+const theme = {
+  mainColor: "#000",
+  subColor: "#ccc",
+  mobile: `(max-width: ${size.mobile})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
+
+export default theme;
