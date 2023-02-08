1852a51b1f6aadca5a43038f1b6c4c61996bbaf0
jasonpark <jasonpark0503@gmail.com>
2023-02-08 17:39:00 +0900

    Design: 각 파트 컴포넌트 삽입 #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainSection from "./components/MainSection";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <MainSection />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
